89364e059b1a6fabc33a0539b44d5ec1c4b2cd7b
Xiaobin <xshen1@statestreet.com>
2019-09-05 09:50:54 +0800

    add test cases

--- dist/VForm.js
+++ dist/VForm.js
@@ -17,6 +17,12 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "d
 
 function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
 
+function ownKeys(object, enumerableOnly) { var keys = Object.keys(object); if (Object.getOwnPropertySymbols) { var symbols = Object.getOwnPropertySymbols(object); if (enumerableOnly) symbols = symbols.filter(function (sym) { return Object.getOwnPropertyDescriptor(object, sym).enumerable; }); keys.push.apply(keys, symbols); } return keys; }
+
+function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i] != null ? arguments[i] : {}; if (i % 2) { ownKeys(source, true).forEach(function (key) { _defineProperty(target, key, source[key]); }); } else if (Object.getOwnPropertyDescriptors) { Object.defineProperties(target, Object.getOwnPropertyDescriptors(source)); } else { ownKeys(source).forEach(function (key) { Object.defineProperty(target, key, Object.getOwnPropertyDescriptor(source, key)); }); } } return target; }
+
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
 function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
@@ -46,8 +52,10 @@ function (_React$Component) {
     _this = _possibleConstructorReturn(this, _getPrototypeOf(VForm).call(this, props));
 
     _this.onMessages = function (messages) {
-      return _this.setState({
-        messages: messages
+      var newMessages = _objectSpread({}, _this.state.messages, {}, messages);
+
+      _this.setState({
+        messages: newMessages
       });
     };
 
--- test/index.test.js
+++ test/index.test.js
@@ -4,10 +4,10 @@ import VForm, { v } from '../dist'
 
 const Field = VForm.fieldFactory(({ name, message, children }) => {
     return (
-        <div>
+        <section>
             {children}
             <div>{message}</div>
-        </div>
+        </section>
     )
 })
 
@@ -57,6 +57,40 @@ function Form(props) {
     )
 }
 
+function FormII(props) {
+    const [values, setValues] = useState({ email: '', marriage: '', birth_date: new Date() })
+
+    const update = value => {
+        validation.test(value, values)
+        setValues({ ...values, ...value })
+    }
+
+    const onSubmit = () => {
+        validation.testAllRules(values)
+    }
+
+    return (
+        <div>
+            <VForm validation={validation} {...props}>
+                <Field name='email'>
+                    <label htmlFor="email">email</label>
+                    <input id="email" onChange={evt => update({ email: evt.target.value })} />
+                </Field>
+
+                <Field name='marriage'>
+                    <label htmlFor="marriage">marriage</label>
+                    <input id="marriage" onChange={evt => update({ marriage: evt.target.value })} />
+                </Field>
+                <Field name='marriage_date'>
+                    <label htmlFor="marriage_date">marriage_date</label>
+                    <input id='marriage_date' onChange={evt => update({ marriage_date: evt.target.value })} />
+                </Field>
+                <button onClick={onSubmit}>Submit</button>
+            </VForm>
+        </div>
+    )
+}
+
 test('basic', () => {
     const { queryByText, getByText } = render(<Form />)
     fireEvent.click(getByText(/submit/i))
@@ -67,25 +101,43 @@ test('basic', () => {
 
 test('Should be email', () => {
     const { queryByText, getByText, getByLabelText } = render(<Form />)
-    fireEvent.change(getByLabelText(/email/i), {target: {value: '2'}})
+    fireEvent.change(getByLabelText(/email/i), { target: { value: '2' } })
     expect(queryByText('Should be email')).toBeTruthy()
-    fireEvent.change(getByLabelText(/email/i), {target: {value: '2@2.com'}})
+    fireEvent.change(getByLabelText(/email/i), { target: { value: '2@2.com' } })
     expect(queryByText('Should be email')).toBeNull()
 })
 
 test('Should be single or married', () => {
     const { queryByText, getByText, getByLabelText } = render(<Form />)
-    fireEvent.change(getByLabelText('marriage'), {target: {value: '2'}})
+    fireEvent.change(getByLabelText('marriage'), { target: { value: '2' } })
     expect(queryByText('Should be single or married')).toBeTruthy()
-    fireEvent.change(getByLabelText('marriage'), {target: {value: 'single'}})
+    fireEvent.change(getByLabelText('marriage'), { target: { value: 'single' } })
     expect(queryByText('Should be single or married')).toBeNull()
 })
 
 test('Required when married', () => {
     const { queryByText, getByText, getByLabelText } = render(<Form />)
-    fireEvent.change(getByLabelText('marriage'), {target: {value: 'married'}})
+    fireEvent.change(getByLabelText('marriage'), { target: { value: 'married' } })
     fireEvent.click(getByText(/submit/i))
     expect(queryByText('Required when married')).toBeTruthy()
-    fireEvent.change(getByLabelText('marriage'), {target: {value: 'single'}})
+    fireEvent.change(getByLabelText('marriage'), { target: { value: 'single' } })
     expect(queryByText('Required when married')).toBeNull()
+})
+
+test('default messages', () => {
+    const { queryByText, getByText, getByLabelText } = render(
+        <FormII defaultMessages={{ email: 'abcd' }} />
+    )
+    expect(queryByText(/abcd/i)).toBeTruthy()
+    fireEvent.click(getByText(/submit/i))
+    expect(queryByText('email is required')).toBeNull()
+})
+
+test('messages', () => {
+    const { queryByText, getByText, getByLabelText } = render(
+        <FormII messages={{ email: 'abcd' }} />
+    )
+    expect(queryByText(/abcd/i)).toBeTruthy()
+    fireEvent.click(getByText(/submit/i))
+    expect(queryByText(/abcd/i)).toBeTruthy()
 })
